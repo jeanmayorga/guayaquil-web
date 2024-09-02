a22533afb4300218f757ef08a29fea7b806ca513
Jean Paul Mayorga <jpmayorga@outlook.com>
2024-09-02 02:49:20 -0500

    fix: adding eerror

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -3,10 +3,15 @@ import scrapMeet2Go from "../../../scripts/scrap-meet2go-events";
 import scrapTicketShow from "../../../scripts/scrap-ticketshow-events";
 
 export async function GET() {
-  console.log("start scrap");
-  await scrapMeet2Go();
-  await scrapTicketShow();
-  console.log("close scrap");
+  try {
+    console.log("start scrap");
+    await scrapMeet2Go();
+    await scrapTicketShow();
+    console.log("close scrap");
 
-  return NextResponse.json({ ok: true });
+    return NextResponse.json({ ok: true });
+  } catch (error) {
+    console.log("Error", error);
+    return NextResponse.json({ ok: false });
+  }
 }
