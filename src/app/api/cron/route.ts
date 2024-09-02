9dc19d242caaaf00e94a23aa21aaac881c1db655
Jean Paul Mayorga <jpmayorga@outlook.com>
2024-09-02 02:54:19 -0500

    fix vron

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,15 +1,17 @@
+export const dynamic = "force-dynamic"; // static by default, unless reading the request
+
 import { NextResponse } from "next/server";
 import scrapMeet2Go from "../../../scripts/scrap-meet2go-events";
 import scrapTicketShow from "../../../scripts/scrap-ticketshow-events";
 
-export async function GET() {
+export async function GET(request: Request) {
   try {
     console.log("start scrap");
     await scrapMeet2Go();
     await scrapTicketShow();
     console.log("close scrap");
 
-    return NextResponse.json({ ok: true });
+    return new Response(`Hello from ${process.env.VERCEL_REGION}`);
   } catch (error) {
     console.log("Error", error);
     return NextResponse.json({ ok: false });
