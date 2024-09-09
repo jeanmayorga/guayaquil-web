035ed2c4ba5e442eaf06bbd09448fc486255b12d
Jean Paul Mayorga <jpmayorga@outlook.com>
2024-09-09 14:18:51 -0500

    fixing cron

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic"; // static by default, unless reading the request
 
 import { NextResponse } from "next/server";
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 import scrapMeet2Go from "../../../scripts/scrap-meet2go-events";
 import scrapTicketShow from "../../../scripts/scrap-ticketshow-events";
 import cleanEvents from "../../../scripts/clean-events";
@@ -13,11 +13,13 @@ export async function GET(request: Request) {
     await scrapTicketShow();
     console.log("close scrap");
 
-    await cleanEvents();
-    console.log("events cleaned.");
+    // await cleanEvents();
+    // console.log("events cleaned.");
 
-    revalidatePath("/");
-    revalidatePath("/events");
+    // revalidatePath("/");
+    // revalidatePath("/events");
+
+    revalidateTag("events");
 
     return new Response(`Hello from ${process.env.VERCEL_REGION}`);
   } catch (error) {
