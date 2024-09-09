7bd542ec5bde660021f8b103317f7cc8c485c3f6
Jean Paul Mayorga <jpmayorga@outlook.com>
2024-09-09 14:52:13 -0500

    fix code

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export function BackButton({ url, text }: Props) {
   return (
-    <div className="my-12" style={{ viewTransitionName: `title` }}>
+    <div className="my-6 md:my-12" style={{ viewTransitionName: `title` }}>
       <Link href={url || "/"}>
         <Button variant="ghost" className="rounded-full">
           <LeftArrowIcon className="h-4 w-4 mr-2" /> {text || "Regresar"}
