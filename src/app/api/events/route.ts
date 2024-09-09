f93b58b16ffc855f4e00fd23cda665a03f3b5596
Jean Paul Mayorga <jpmayorga@outlook.com>
2024-09-09 01:45:41 -0500

    bad route

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { supabase } from "@/lib/supabase";
 
+export const dynamic = "force-dynamic";
+
 function getJustDate(date: Date) {
   const stringDate = date.toISOString();
   return stringDate.split("T")[0];
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -38,8 +38,6 @@ export const metadata: Metadata = {
   },
 };
 
-export const dynamic = "force-dynamic";
-
 export default async function Home({
   searchParams,
 }: {
