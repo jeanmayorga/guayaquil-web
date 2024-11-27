0625b52f0665b1b13d68fe72f8776f55b7e53df2
Jean Paul Mayorga <jpmayorga@outlook.com>
2024-11-27 01:27:34 -0500

    adding sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,21 +1,19 @@
 import type { MetadataRoute } from "next";
-import { getEvents } from "./events/services";
+import { getEvents } from "./events/actions";
 
 export const revalidate = 3600;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const domain = "https://www.guayaquil.app";
 
-  const eventsResponse = await getEvents({ limit: 200, log: "sitemap" });
+  const events = await getEvents({ limit: 200 });
 
-  const eventPages: MetadataRoute.Sitemap = eventsResponse.events.map(
-    (event) => ({
-      url: `${domain}/events/${event.slug}`,
-      lastModified: event.last_updated,
-      changeFrequency: "daily",
-      priority: 1,
-    })
-  );
+  const eventPages: MetadataRoute.Sitemap = events.map((event) => ({
+    url: `${domain}/events/${event.slug}`,
+    lastModified: event.last_updated,
+    changeFrequency: "daily",
+    priority: 1,
+  }));
 
   return [
     {
